cbf0ff0232b8d16a7feae9634f44f4ab17134931
Alexey <zabulonov444@yandex.ru>
2022-11-14 21:16:37 +0300

    first build!

---
+++ ClientsCRUD/ClientsCRUD/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using ClientsCRUD.Models;
+
+
+namespace ClientsCRUD.Data
+{
+	public class ApplicationDbContext : DbContext
+	{
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+
+        }
+        public DbSet<Clients> clients { get; set; } = null!;
+    }
+}
+
---
+++ ClientsCRUD/ClientsCRUD/Models/Clients.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace ClientsCRUD.Models
+{
+	public class Clients
+	{
+		[Key]
+		public int Id { get; set; }
+
+		[Required]
+		public string FirstName { get; set; } = "";
+
+        [Required]
+        public string LastName { get; set; } = "";
+
+        [Required]
+        public string PhoneNumber { get; set; } = "";
+
+        public string Email { get; set; } = ""; 
+    }
+}
+

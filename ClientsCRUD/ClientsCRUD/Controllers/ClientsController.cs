0e37e6c0969eeccd19ac69797be9a70c2c7f6491
Alexey <zabulonov444@yandex.ru>
2022-11-20 22:30:17 +0300

    add Create

--- ClientsCRUD/ClientsCRUD/Controllers/ClientsController.cs
+++ ClientsCRUD/ClientsCRUD/Controllers/ClientsController.cs
@@ -7,18 +7,39 @@ namespace ClientsCRUD.Controllers
 {
 	public class ClientsController : Controller
 	{
-		private readonly ApplicationDbContext _dbContext;
+		private readonly ApplicationDbContext _db;
 
 		public ClientsController(ApplicationDbContext  context)
 		{
-			_dbContext = context;
+			_db = context;
 		}
 
+		// GET Controller
 		public IActionResult Index()
 		{
-			List<Clients> clients = _dbContext.clients.ToList();
+			List<Clients> clients = _db.clients.ToList();
 			return View(clients);
 		}
+
+		// GET: Create
+		public IActionResult Create()
+		{
+			return View();
+		}
+
+		// POST : Create
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public IActionResult Create(Clients obj)
+		{
+			if(ModelState.IsValid)
+			{
+				_db.Add(obj);
+				_db.SaveChanges();
+				return RedirectToAction("Index");
+			}
+            return View(obj);
+        }
 	}
 }
 
